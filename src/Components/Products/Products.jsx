2473debf6f3c3eb81e8e53c05ab8cb56260f5b33
Ajay-1502 <ahdesai40@gmail.com>
2025-08-18 19:47:20 +0530

    Feature : Display products on with product details and image

--- src/Components/Products/Products.jsx
+++ src/Components/Products/Products.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import ProductList from './Products';
+import { GoHeartFill } from 'react-icons/go';
 
 const Products = () => {
   const categories = [
@@ -11,6 +13,53 @@ const Products = () => {
   ];
 
   const [activeTab, setActiveTab] = useState('All');
+
+  const renderProducts = ProductList.map((product) => {
+    return (
+      //Card
+      <div className="bg-zinc-100 p-5 border-1 border-zinc-300 rounded-lg">
+        <div className="flex justify-between items-center">
+          <button className="text-3xl text-zinc-300">
+            <GoHeartFill />
+          </button>
+          <div>
+            {(product.onSale || product.newArrival) && (
+              <span
+                className={`px-3 py-1 text-white ${
+                  product.onSale ? 'bg-red-600' : 'bg-green-600'
+                }`}
+              >
+                {product.onSale ? 'Sale' : 'New'}
+              </span>
+            )}
+          </div>
+        </div>
+
+        {/*Product Image*/}
+        <div className=" w-full h-[30vh]">
+          <img src={product.image} alt="product-image" />
+        </div>
+
+        {/*Product Detail*/}
+        <div className="text-center mt-3">
+          <h3 className="text-[1.4rem] font-semibold">{product.name}</h3>
+          <div className="mt-1 mb-5">
+            {product.onSale && (
+              <span className="text-zinc-600 font-semibold text-lg line-through mr-8">
+                ${product.oldPrice.toFixed(2)}
+              </span>
+            )}
+            <span className="text-red-600 font-semibold text-lg">
+              ${product.price.toFixed(2)}
+            </span>
+          </div>
+          <button className="bg-blue-600 text-white text-lg py-3 w-full rounded-lg cursor-pointer active:bg-blue-700">
+            Add to Cart
+          </button>
+        </div>
+      </div>
+    );
+  });
 
   return (
     <section className="max-w-[1300px] mx-auto px-12 py-12">
@@ -35,7 +84,7 @@ const Products = () => {
       </div>
 
       {/*Product listing*/}
-      <div></div>
+      <div className="grid grid-cols-4 gap-9 mt-12">{renderProducts}</div>
     </section>
   );
 };
